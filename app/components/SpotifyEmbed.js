0b703f2d5cfa5f668c33e583375301b7959b83f4
Angelo Mendoza <angelo@modiphy.net>
2025-03-17 12:10:14 +0800

    feat: enhance SpotifyEmbed component with loading state
    - added loading state to display a loading message while the iframe is loading
    - refactored iframe loading logic to improve user experience
    - updated the component to dynamically generate the Spotify embed URL based on episodeUri

--- app/components/SpotifyEmbed.js
+++ app/components/SpotifyEmbed.js
@@ -1,24 +1,45 @@
 "use client";
 
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 
 export default function SpotifyEmbed({ episodeUri }) {
+  const [isLoading, setIsLoading] = useState(true);
+  const spotifyUrl = episodeUri
+    ? `https://open.spotify.com/embed/episode/${episodeUri
+        .split(":")
+        .pop()}?utm_source=generator`
+    : "https://open.spotify.com/embed/episode/0kUSu8Ln3vvy4tSk84kEKe?utm_source=generator";
+
+  // Handle iframe load event
+  const handleIframeLoad = () => {
+    setIsLoading(false);
+  };
+
+  // Reset loading state when episode changes
   useEffect(() => {
-    window.onSpotifyIframeApiReady = (IFrameAPI) => {
-      const element = document.getElementById("embed-iframe");
-      const options = {
-        uri: episodeUri,
-        height: "152",
-      };
-      const callback = (EmbedController) => {};
-      IFrameAPI.createController(element, options, callback);
-    };
+    setIsLoading(true);
   }, [episodeUri]);
 
   return (
-    <>
-      <script src="https://open.spotify.com/embed/iframe-api/v1" async></script>
-      <div id="embed-iframe" className="w-full h-[152px]" />
-    </>
+    <div className="relative w-full h-[152px]">
+      {isLoading && (
+        <div className="absolute inset-0 bg-primary rounded-xl flex items-center justify-center">
+          <div className="flex flex-col items-center">
+            <p className="mt-2 text-sm text-white">Loading...</p>
+          </div>
+        </div>
+      )}
+      <iframe
+        src={spotifyUrl}
+        width="100%"
+        height="152"
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+        loading="lazy"
+        onLoad={handleIframeLoad}
+        className={`rounded ${
+          isLoading ? "opacity-0" : "opacity-100"
+        } transition-opacity duration-300`}
+      ></iframe>
+    </div>
   );
 }
