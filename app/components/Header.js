0db166648139b74981116f2479d3dc69fe2be228
Angelo Mendoza <angelo@modiphy.net>
2025-03-18 13:49:10 +0800

    style: enhance mobile menu transitions and accessibility
    - added transition effects to the mobile menu for smoother opening and closing
    - updated the Dialog component to include aria-hidden attribute for better accessibility
    - adjusted padding and margin for improved layout and spacing in the mobile menu

--- app/components/Header.js
+++ app/components/Header.js
@@ -52,10 +52,15 @@ export default function Example() {
       <Dialog
         open={mobileMenuOpen}
         onClose={setMobileMenuOpen}
-        className="lg:hidden"
+        transition
+        className="lg:hidden transition duration-300 data-[closed]:opacity-0 data-[closed]:pointer-events-none"
       >
-        <div className="fixed inset-0 z-10" />
-        <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white py-8 px-gutter sm:max-w-sm sm:ring-1 sm:ring-white/10">
+        <div
+          className="fixed inset-0 z-10"
+          aria-hidden="true"
+          onClick={() => setMobileMenuOpen(false)}
+        />
+        <DialogPanel className="fixed rounded-lg top-4 right-2 left-2 px-4 py-4 sm:left-auto sm:right-2 z-10 bg-white shadow-lg outline outline-primary sm:min-w-sm sm:ring-1 sm:ring-white/10 md:right-6">
           <div className="flex items-center justify-between">
             <Link
               href="/"
@@ -68,13 +73,13 @@ export default function Example() {
             <button
               type="button"
               onClick={() => setMobileMenuOpen(false)}
-              className="-m-2.5 rounded-md p-2.5 text-dark hover:text-primary transition-colors duration-300 cursor-pointer"
+              className="-m-2.5 rounded-md p-2.5 text-primary hover:text-dark transition-colors duration-300 cursor-pointer"
             >
               <span className="sr-only">Close menu</span>
               <HiX aria-hidden="true" className="size-6" />
             </button>
           </div>
-          <div className="mt-10 flow-root">
+          <div className="mt-8 flow-root">
             <div className="-my-6 divide-y divide-gray-500/10">
               <div className="space-y-6 py-6">
                 {navigation.map((item) => (
